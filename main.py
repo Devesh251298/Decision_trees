85b39633c1524925be6e53c725c7143cd7553948
Mireia Hernandez Caralt <mh1022@ic.ac.uk>
2022-10-20 20:31:15 +0100

    Updated find_split function to find split that maximizes information gain in the dataset.

--- main.py
+++ main.py
@@ -26,23 +26,66 @@ def calculate_entropy(dataset):
     Returns:
         entropy (float)
     """
+    return 0
 
 
 def find_split(dataset):
-    """Load data.
-
-    Extended description of function.
+    """Find split that maximizes information gain in dataset.
 
     Args:
-        filename (str): 
+        dataset (np.ndarray, size N x 8): 
+            dataset[:,:7] -> attributes (float)
+            dataset[:,7] -> label (int from 1 to 4)
+        
 
     Returns:
-        attribute (int): integer going from 
-        value (float): 
-        left_dataset (np.ndarray, shape Nleft x 8)
-        right_dataset (np.ndarray, shape Nright x 8)
-
+        split_attribute (int): integer from 0 to 6 (both included)
+        split_value (float): value used to split the chosen split_attribute
+        split_left_dataset (np.ndarray, shape Nleft x 8)
+        split_right_dataset (np.ndarray, shape (N-Nleft) x 8)
     """
+    # get the number of attributes in dataset (= 7)
+    N, num_attributes = dataset.shape[0], dataset.shape[1] - 1
+    # calculate the entropy of the entire dataset
+    dataset_entropy = calculate_entropy(dataset)
+    
+    # initialize the target variables defining split
+    max_info_gain = -1
+    
+    # iterate over all attributes to find the splitting attribute
+    for attribute in range(num_attributes):
+        # get values of that attribute
+        values = dataset[:, attribute]
+        
+        # get indices of the dataset sorted according to that attribute
+        sorted_indices = np.argsort(values)
+        
+        # iterate from smallest to largest value and find optimal splitting point
+        for i, index in enumerate(sorted_indices):
+            value = dataset[index, attribute]
+            
+            # split dataset on the condition
+            # left_dataset -> attribute <= value
+            # right_dataset ->  attribute > value
+            left_dataset = dataset[sorted_indices[:i+1]]
+            right_dataset = dataset[sorted_indices[i+1:]]
+
+            # calculate left and right entropies
+            left_entropy = calculate_entropy(left_dataset)
+            right_entropy = calculate_entropy(right_dataset)
+            
+            # calculate information gain
+            info_gain = dataset_entropy - (i/N * left_entropy + (N-i)-N * right_entropy)
+            
+            # if information gain is larger than the current maximum, update variables
+            if info_gain > max_info_gain:
+                max_info_gain = info_gain
+                split_attribute, split_value = attribute, value
+                split_left_dataset = left_dataset
+                split_right_dataset = right_dataset
+    
+    return split_attribute, split_value, split_left_dataset, split_right_dataset
+
 
 def decision_tree_learning(training_dataset, depth):
     """Load data.
