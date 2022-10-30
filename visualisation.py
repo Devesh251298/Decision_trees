0cd601e96dd6ec8aacac6eb9350e9d4147085a60
Devesh Joshi <deveshjoshi@Deveshs-MacBook-Air.local>
2022-10-30 19:31:11 +0000

    Grid Visualization

--- visualisation.py
+++ visualisation.py
@@ -8,7 +8,7 @@ from matplotlib import colors as mcolors
 
 # Need to find the split_value 
 
-def visualise_decision_tree(node, tree, ax, x, y, max_x=5, max_y=5, depth=0, max_depth=8):
+def visualise_decision_tree(node, tree, ax, x, y, grid = [], grid_x = [], grid_y = [], max_x=5, max_y=5, depth=0, max_depth=3):
 
     kwargs = dict(
                 bbox=dict(boxstyle='round', facecolor='white', edgecolor='b'),
@@ -18,62 +18,79 @@ def visualise_decision_tree(node, tree, ax, x, y, max_x=5, max_y=5, depth=0, max
                 fontsize=8
                 )
     segments = []
-    
-    if depth <= max_depth:
+
+    dim_x = grid_x[y,x]
+    dim_y = grid_y[y,x]
+    if depth < max_depth:
             if node.parent is None:
                 # root
                 text = f'Feature: {node.attribute} \n Split at: {node.value}'
-                ax.annotate(text, xy=(x,y), **kwargs)
+                ax.annotate(text, xy=(dim_x,dim_y), **kwargs)
 
-
-            if node.left != None:
-                
+            if node.left != None:         
                 if node.left.leaf:
-                    xl = x - 0.1
-                    yl = y - 0.1   
-                    segments.append([[x, y], [xl, yl]]) 
+
+                    xl = x - np.power(2,(max_depth-depth-1))
+                    yl = y - 1
+                    dim_xl = grid_x[yl,xl]
+                    dim_yl = grid_y[yl,xl]
+
+                    segments.append([[dim_x, dim_y], [dim_xl, dim_yl]]) 
                     text = f'Leaf: {node.left.label}'
                     kwargs['bbox']['facecolor']='green'
-                    ax.annotate(text, xy=(xl,yl), **kwargs)
+                    ax.annotate(text, xy=(dim_xl,dim_yl), **kwargs)
                     line_segments = LineCollection(segments, linewidths=1, linestyle='solid')
                     ax.add_collection(line_segments)
-                    segments.remove([[x, y], [xl, yl]])
+                    segments.remove([[dim_x, dim_y], [dim_xl, dim_yl]])
                     
                     
                 else:
-                    xl = x - 0.5
-                    yl = y - 0.1    
-                    segments.append([[x, y], [xl, yl]]) 
+                    xl = x - np.power(2,(max_depth-depth-1))
+                    yl = y - 1   
+                    dim_xl = grid_x[yl,xl]
+                    dim_yl = grid_y[yl,xl]
+
+                    segments.append([[dim_x, dim_y], [dim_xl, dim_yl]]) 
                     text = f'Feature: {node.left.attribute}\n Split at: {node.left.value}'
-                    ax.annotate(text, xy=(xl,yl), **kwargs)
+                    ax.annotate(text, xy=(dim_xl,dim_yl), **kwargs)
                     kwargs['bbox']['facecolor']='white'
                     line_segments = LineCollection(segments, linewidths=1, linestyle='solid')
                     ax.add_collection(line_segments)
-                    visualise_decision_tree(node.left, tree, ax, xl, yl, max_x, max_y, depth=depth + 1)
+                    visualise_decision_tree(node.left, tree, ax, xl, yl, grid, grid_x, grid_y,max_x, max_y, depth=depth + 1, max_depth=max_depth)
                    
             if node.right != None:
-                
                 if node.right.leaf:
-                    xr = x + 0.1
-                    yr = y - 0.1 
-                    segments.append([[x, y], [xr, yr]])
+                    xr = x + np.power(2,(max_depth-depth-1))
+                    yr = y - 1
+
+                    dim_xr = grid_x[yr,xr]
+                    dim_yr = grid_y[yr,xr]
+
+                    segments.append([[dim_x, dim_y], [dim_xr, dim_yr]])
                     text = f'Leaf: {node.right.label}'
                     kwargs['bbox']['facecolor']='green'
-                    ax.annotate(text, xy=(xr,yr), **kwargs)
+                    ax.annotate(text, xy=(dim_xr,dim_yr), **kwargs)
                     line_segments = LineCollection(segments, linewidths=1, linestyle='solid')
-                    segments.remove([[x, y], [xr, yr]])
+                    segments.remove([[dim_x, dim_y], [dim_xr, dim_yr]])
                     ax.add_collection(line_segments)
                     
                 else:
-                    xr = x + 0.5
-                    yr = y - 0.1
-                    segments.append([[x, y], [xr, yr]])
+                    xr = x + np.power(2,(max_depth-depth-1))
+                    yr = y - 1
+
+                    dim_xr = grid_x[yr,xr]
+                    dim_yr = grid_y[yr,xr]
+                    print(xr,yr,max_depth,depth)
+
+                    segments.append([[dim_x, dim_y], [dim_xr, dim_yr]])
                     text = f'Feature: {node.right.attribute} \n Split at:{node.right.value}'
                     kwargs['bbox']['facecolor']='white'
-                    ax.annotate(text, xy=(xr,yr), **kwargs)
+                    ax.annotate(text, xy=(dim_xr,dim_yr), **kwargs)
                     line_segments = LineCollection(segments, linewidths=1, linestyle='solid')
                     ax.add_collection(line_segments)
-                    visualise_decision_tree(node.right, tree, ax, xr, yr, max_x, max_y, depth=depth + 1)
+                    visualise_decision_tree(node.right, tree, ax, xr, yr, grid, grid_x, grid_y,max_x, max_y, depth=depth + 1,max_depth=max_depth)
+
+    # print('Left : ',left_pos,' Right : ',right_pos)
                     
 
 def test_visualise():
@@ -83,13 +100,21 @@ def test_visualise():
     # clf = DecisionTreeClassifier(criterion = "entropy")
     # clf.fit(dataset[:,:-1], dataset[:,-1])
     # tree.plot_tree(clf)
+    max_depth = 6
+    grid = np.zeros((max_depth+1,np.power(2,max_depth+1)))
+    grid_x = np.zeros((max_depth+1,np.power(2,max_depth+1)))
+    grid_y = np.zeros((max_depth+1,np.power(2,max_depth+1)))
+    for i in range(max_depth+1):
+        grid_y[i,:] = 0.1*i
+        for j in range(len(grid_x[i])):
+            grid_x[i,j] = 0.05*(j - int(np.power(2,max_depth+1)/2))
 
     dtree = DecisionTree_Classifier()
     dtree.fit(dataset)
     
     
     fig, ax = plt.subplots(figsize=(10,10))
-    visualise_decision_tree(node=dtree.dtree, tree=dtree, x=0, y=1, ax=ax, max_depth=8, max_x=6, max_y=5)
+    visualise_decision_tree(node=dtree.dtree, tree=dtree, grid = grid, grid_x = grid_x, grid_y = grid_y,x=np.power(2,max_depth), y=max_depth, ax=ax, max_depth=max_depth, max_x=6, max_y=5)
     ax.set_ylim(0.25,1.1)
     ax.set_xlim(-2,2)
     ax.axis('off')
